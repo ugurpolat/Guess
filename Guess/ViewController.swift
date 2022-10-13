8a8e713f250b28c17cfde26952b438dff5c9d150
Ugur Polat <ugurpolat113@gmail.com>
2022-10-13 21:07:39 +0300

    first commit"
    "Initial commit

---
+++ Guess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess
+//
+//  Created by Ugur Polat on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
