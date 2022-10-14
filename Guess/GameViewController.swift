9824612fa43f6902ed5f677b16befafc02da43a0
Ugur Polat <ugurpolat113@gmail.com>
2022-10-14 10:18:59 +0300

    Add result page

--- Guess/GameViewController.swift
+++ Guess/GameViewController.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    let myGuess = "20";
+    var lives = 1;
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func guessClicked(_ sender: Any) {
+        checkMyGuess(guessNumber: guessTextField.text!)
     }
-    */
-
+    
+    @IBAction func cleanPlaceHolder(_ sender: UITextField) {
+        sender.placeholder = ""
+    }
+    
+    func checkMyGuess(guessNumber:String) {
+        if guessNumber != "" {
+            if guessNumber != myGuess {
+                lives -= 1
+                guessTextField.text = "";
+                guessLabel.text = "\(lives) lives left"
+                guessTextField.placeholder = ""
+                if lives == 0 {
+                    
+                    changePage(result: ":(")
+                }
+            } else {
+                changePage(result:":)")
+            }
+            
+        } else {
+            warnPlaceHolder()
+        }
+    }
+    
+    
+    func changePage(result:String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let targetViewController = storyboard.instantiateViewController(withIdentifier: "target") as! TargetViewController
+        
+        let sendMessage:String = result
+        targetViewController.msg = sendMessage
+        self.navigationController?.pushViewController(targetViewController, animated: true)
+    }
+    
+    func warnPlaceHolder(){
+        let placeholder = "Your guess"
+        guessTextField.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.red]
+        )
+    }
 }
+
